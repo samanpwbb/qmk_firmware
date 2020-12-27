2ad9caa6b17286f49a845e7284f37ce3b90881a9
samanpwbb <samanpwbb@gmail.com>
2020-12-27 15:31:07 -0800

    Add artwork

--- keyboards/lily58/keymaps/saman/rules.mk
+++ keyboards/lily58/keymaps/saman/rules.mk
@@ -1,13 +1,13 @@
 EXTRAKEY_ENABLE = yes
 RGBLIGHT_ENABLE = yes
 ENCODER_ENABLE = yes
-MOUSEKEY_ENABLE = yes
+OLED_DRIVER_ENABLE = yes
 
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/rgb_state_reader.c \
         ./lib/layer_state_reader.c \
-        ./lib/logo_reader.c \
         ./lib/keylogger.c \
+        # ./lib/logo_reader.c \
         # ./lib/mode_icon_reader.c \
         # ./lib/host_led_state_reader.c \
         # ./lib/timelogger.c \
