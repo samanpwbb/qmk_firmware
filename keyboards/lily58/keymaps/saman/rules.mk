8ae8a35e1e135da1140d8166e2e42372681fe300
samanpwbb <samanpwbb@gmail.com>
2021-01-30 22:04:36 -0800

    Add design layer, abandon emojis, add new art and swap art based on layers

--- keyboards/lily58/keymaps/saman/rules.mk
+++ keyboards/lily58/keymaps/saman/rules.mk
@@ -2,7 +2,6 @@ EXTRAKEY_ENABLE = yes
 RGBLIGHT_ENABLE = false
 ENCODER_ENABLE = yes
 OLED_DRIVER_ENABLE = yes
-UNICODEMAP_ENABLE = yes
 
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/layer_state_reader.c \
